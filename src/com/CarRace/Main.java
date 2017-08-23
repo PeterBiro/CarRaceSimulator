e8322ae204bd9d61ce45237c02260d53f2a5dfb2
Péter Biró <biro.peter.hu@gmail.com>
2017-08-23 21:39:58 +0200

    isRaining generated

--- src/com/CarRace/Main.java
+++ src/com/CarRace/Main.java
@@ -1,8 +1,40 @@
 package com.CarRace;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
+    /*************
+     * constants *
+     *************/
+    static final int raceRounds = 50;
+    static final int chanceOfRain = 30;
+
+
+    /*
+    Generating weather conditions for the whole race
+     */
+    static ArrayList<Boolean> isRaining = new ArrayList<Boolean>() {{
+        Random rand = new Random();
+        for (int i = 0; i <raceRounds ; i++) {
+            add(rand.nextInt(100) < chanceOfRain);
+        }
+    }};
+
+
+    //30% chance of rain every hour
+    /*
+    createVehicles() // creates 10 cars, 10 trucks and 10 motorcycles
+    simulateRace() // simulates the race by calling moveForAnHour() on every vehicle 50 times and setting whether its raining.
+    printRaceResults() // prints each vehicle's name, distance traveled ant type.
+    */
+
     public static void main(String[] args) {
-	// write your code here
+
+        //createVehicles(); // creates 10 cars, 10 trucks and 10 motorcycles
+        //simulateRace(); // simulates the race by calling moveForAnHour() on every vehicle 50 times and setting whether its raining.
+        //printRaceResults(); // prints each vehicle's name, distance traveled ant type.
+
     }
 }
