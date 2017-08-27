472b4e83a2da6365fe77dddd267595b791b70b9e
Péter Biró <biro.peter.hu@gmail.com>
2017-08-27 18:15:53 +0200

    Motorcycles are racing :)

--- src/com/CarRace/Main.java
+++ src/com/CarRace/Main.java
@@ -11,6 +11,8 @@ public class Main {
     static final int raceRounds = 50;
     static final int chanceOfRain = 30;
 
+    static ArrayList<Motorcycle> motorcyclesList = new ArrayList<>();
+
 
     /*
     Generating weather conditions for the whole race
@@ -25,30 +27,37 @@ public class Main {
     public static void createVehicles(){
 
         //creating motorcycles
-        ArrayList<Motorcycle> motorcyclesList = new ArrayList<>();
         for (int i = 0; i < 10 ; i++) {
             Motorcycle tmpBike = new Motorcycle();
             motorcyclesList.add(tmpBike);
         }
-        
+
+        //
     }
-    /*
-    createVehicles() // creates 10 cars, 10 trucks and 10 motorcycles
-    simulateRace() // simulates the race by calling moveForAnHour() on every vehicle 50 times and setting whether its raining.
-    printRaceResults() // prints each vehicle's name, distance traveled ant type.
-    */
+
+    public static void simulateRace() {
+
+        for (int i = 0; i < raceRounds ; i++) {
+            for (Motorcycle bike: motorcyclesList) {
+                bike.moveForAnHour(isRaining.get(i));
+            }
+
+        }
+    }
+
+    public static void printRaceResults() {
+
+        for (Motorcycle bike:motorcyclesList) {
+            System.out.println("The motorcycle: '" + bike.name + "' got " + bike.distanceTravelled +" km far");
+        }
+    }
 
     public static void main(String[] args) {
-
+        //Main app = new Main();
         createVehicles();
-        //simulateRace(); // simulates the race by calling moveForAnHour() on every vehicle 50 times and setting whether its raining.
-        //printRaceResults(); // prints each vehicle's name, distance traveled ant type.
+        simulateRace(); // simulates the race by calling moveForAnHour() on every vehicle 50 times and setting whether its raining.
+        printRaceResults(); // prints each vehicle's name, distance traveled ant type.
 
     }
 }
 
-// speed is 100km/h. If rains, travels with 5-50km/h slower (randomly).
-// static nameNumber // The number of the instance created. Used for its name.
-// name // Are called "Motorcycle 1", "Motorcycle 2", "Motorcycle 3",... Unique.
-// distanceTraveled
-// moveForAnHour()
